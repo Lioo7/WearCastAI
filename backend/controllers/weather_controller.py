36409d3a544424a1dfd482f2ded3140027f04033
Lioz Akirav <lioz.akirav@gmail.com>
2024-03-11 17:31:36 +0200

    add fetch_weather_data_by_city endpoint

--- backend/controllers/weather_controller.py
+++ backend/controllers/weather_controller.py
@@ -1,10 +1,10 @@
 from flask import Blueprint, jsonify, request, abort
-
-from services.weather_service import fetch_weather_data_by_coordinates
+from services.weather_service import fetch_weather_data_by_coordinates, fetch_weather_data_by_city
 
 blueprint = Blueprint('weather', __name__, url_prefix='/api/v1')
 
 
+# example: http://127.0.0.1:5000/api/v1/weather-by-coordinates?latitude=32.085300&longitude=34.781769#
 @blueprint.route('/weather-by-coordinates', methods=['GET'])
 def get_weather_by_coordinates():
     latitude = request.args.get('latitude')
@@ -20,4 +20,16 @@ def get_weather_by_coordinates():
     return jsonify(weather_data), 200
 
 
-# Other endpoints implementation...
+# example: http://127.0.0.1:5000/api/v1/weather-by-city?city=Tel-Aviv
+@blueprint.route('/weather-by-city', methods=['GET'])
+def get_weather_by_city():
+    city = request.args.get('city')
+
+    if not city:
+        abort(400, 'Missing required parameter')
+
+    weather_data = fetch_weather_data_by_city(city)
+    if weather_data is None:
+        abort(404, 'Failed to fetch weather data')
+
+    return jsonify(weather_data), 200
--- backend/services/weather_service.py
+++ backend/services/weather_service.py
@@ -1,11 +1,23 @@
 import requests
 from config import settings
 
+api_key = settings.OPENWEATHERMAP_API_KEY
 
-def fetch_weather_data_by_coordinates(latitude, longitude):
-    api_key = settings.OPENWEATHERMAP_API_KEY
+
+def fetch_weather_data_by_coordinates(latitude, longitude, units='metric', cnt=8):
     # one unit of 'cnt' equals to 3 hours
-    url = f'http://api.openweathermap.org/data/2.5/forecast?lat={latitude}&lon={longitude}&appid={api_key}&units=metric&cnt=8'
+    url = f'http://api.openweathermap.org/data/2.5/forecast?lat={latitude}&lon={longitude}&appid={api_key}&units={units}&cnt={cnt}'
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return None
+
+
+def fetch_weather_data_by_city(city, units='metric', cnt=8):
+    # one unit of 'cnt' equals to 3 hours
+    url = f'http://api.openweathermap.org/data/2.5/forecast?q={city}&appid={api_key}&units={units}&cnt={cnt}'
     response = requests.get(url)
 
     if response.status_code == 200:
